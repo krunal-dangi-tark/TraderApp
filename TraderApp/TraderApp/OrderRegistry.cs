69e8cdc3ed6b709875cae39dbaac05c5a277c759
krunal-dangi-tark <krunal.dangi@tarktech.com>
2024-04-16 17:43:04 +0530

    Added Order and stock registry

---
+++ TraderApp/TraderApp/OrderRegistry.cs
@@ -0,0 +1,11 @@
+﻿using TraderApp.Entities;
+
+namespace TraderApp
+{
+    internal class OrderRegistry
+    {
+        private static readonly List<TradeOrder> tradeOrders = new List<TradeOrder>();
+        public List<TradeOrder> GetOrderByType(Enum.OrderType orderType) => tradeOrders.Where(x => x.OrderType.Equals(orderType)).ToList();
+        public void PlaceOrder(TradeOrder order) => tradeOrders.Add(order);
+    }
+}
---
+++ TraderApp/TraderApp/StockRegistry.cs
@@ -0,0 +1,12 @@
+﻿using TraderApp.Entities;
+
+namespace TraderApp
+{
+    internal class StockRegistry
+    {
+        private static readonly List<Stock> stocks = new List<Stock>();
+
+        public Stock? GetStockByName(string stockName) => stocks.FirstOrDefault(x => x.Name.Equals(stockName)); 
+        public void AddStock(Stock stock) => stocks.Add(stock);
+    }
+}
